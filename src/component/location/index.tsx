b9bc72244cfd167077c40d9a509517efe330c8b9
yspark <yspark@ictpeople.co.kr>
2026-05-28 14:10:41 +0900

    Initial Commit

---
+++ src/component/location/index.tsx
@@ -0,0 +1,66 @@
+import { Map } from "./map"
+import CarIcon from "../../icons/car-icon.svg?react"
+import BusIcon from "../../icons/bus-icon.svg?react"
+import { LazyDiv } from "../lazyDiv"
+import { LOCATION, LOCATION_ADDRESS } from "../../const"
+
+/**
+ * 오시는 길 정보를 표시하는 컴포넌트입니다.
+ * 지도와 대중교통, 자가용 이용 방법을 안내합니다.
+ *
+ * @returns {JSX.Element} 오시는 길 섹션
+ */
+export const Location = () => {
+  return (
+    <>
+      {/* 지도 및 주소 섹션 */}
+      <LazyDiv className="card location">
+        <h2 className="english">Location</h2>
+        <div className="addr">
+          {LOCATION}
+          <div className="detail">{LOCATION_ADDRESS}</div>
+        </div>
+        <Map />
+      </LazyDiv>
+
+      {/* 대중교통 및 자가용 안내 섹션 */}
+      <LazyDiv className="card location">
+        {/* 대중교통 안내 */}
+        <div className="location-info">
+          <div className="transportation-icon-wrapper">
+            <BusIcon className="transportation-icon" />
+          </div>
+          <div className="heading">대중교통</div>
+          <div />
+          <div className="content">
+            * 전철 이용 시
+            <br />
+            <b>KTX 광명역 / 1호선 광명역</b> 하차
+            <br />
+            → 1번 또는 3번 출구 이용 또는 도보 약 5분 거리
+          </div>
+          <div />          
+        </div>
+
+        {/* 자가용 안내 */}
+        <div className="location-info">
+          <div className="transportation-icon-wrapper">
+            <CarIcon className="transportation-icon" />
+          </div>
+          <div className="heading">자가용 이용 시</div>
+          <div />
+          <div className="content">
+            네이버 지도, 카카오 네비, 티맵 등 이용
+            <br />
+            <b>아이벡스컨벤션</b> 또는 <b>AK플라자(광명)</b> 검색
+            <br />
+            - <b>AK플라자 지하 주차장</b>에 주차하시면 됩니다.
+            <br />
+            (컨벤션은 <b>AK플라자 5층</b>)
+          </div>
+          <div />          
+        </div>
+      </LazyDiv>
+    </>
+  )
+}
